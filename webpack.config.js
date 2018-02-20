5b0303a61fb4468a7f833591b0230157b28a59ff
Richard Fox <richard.fox@ahref.co.uk>
2018-02-20 03:25:44 +0100

    stuff, i guess?

--- webpack.config.js
+++ webpack.config.js
@@ -2,4 +2,9 @@ const slsw = require('serverless-webpack');
 
 module.exports = {
   entry: slsw.lib.entries,
+  output: {
+    libraryTarget: 'commonjs',
+    path: path.resolve(__dirname, '.webpack'),
+    filename: '[name].js',
+  },
 };
