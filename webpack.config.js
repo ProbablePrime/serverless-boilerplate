8ed27dd5f8c2d5fe28a06912a5d0e8eb982c77c6
Richard Fox <richard.fox@ahref.co.uk>
2018-02-20 03:34:54 +0100

    update deps kick of prettier

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const slsw = require('serverless-webpack');
 
 module.exports = {
-  entry: slsw.lib.entries,
-  output: {
-    libraryTarget: 'commonjs',
-    path: path.resolve(__dirname, '.webpack'),
-    filename: '[name].js',
-  },
+    entry: slsw.lib.entries,
+    output: {
+        libraryTarget: 'commonjs',
+        path: path.resolve(__dirname, '.webpack'),
+        filename: '[name].js',
+    },
 };
