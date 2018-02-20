e9ac24570f3f964e89c73e6c30411ce5d859e5a9
Richard Fox <richard.fox@ahref.co.uk>
2018-02-20 04:00:50 +0100

    add atl

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,24 @@
 const slsw = require('serverless-webpack');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const { CheckerPlugin } = require('awesome-typescript-loader');
 
 module.exports = {
     entry: slsw.lib.entries,
+    resolve: {
+        extensions: ['.ts'],
+    },
     output: {
         libraryTarget: 'commonjs',
         path: path.resolve(__dirname, '.webpack'),
         filename: '[name].js',
     },
-    plugins: [
-        new UglifyJSPlugin(),
-    ]
+    plugins: [new UglifyJSPlugin(), new CheckerPlugin()],
+    module: {
+        loaders: [
+            {
+                test: /\.ts$/,
+                loader: 'awesome-typescript-loader',
+            },
+        ],
+    },
 };
